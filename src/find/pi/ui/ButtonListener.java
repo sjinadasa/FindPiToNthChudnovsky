8079ff5aa4a527c07971a118ceae9784236b4657
Sachintha Jinadasa <sjinadasa@192.168.20.8>
2019-07-03 11:55:32 +1000

    Added Progress Bar Added Creator Message and Last Update Date Learned about using Threads

--- src/find/pi/ui/ButtonListener.java
+++ src/find/pi/ui/ButtonListener.java
@@ -8,61 +8,106 @@ package find.pi.ui;
 import find.pi.logic.findPiToDecimalPlace;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import static java.lang.Thread.sleep;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.lang.Thread.State;
 import javax.swing.JButton;
+import javax.swing.JProgressBar;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.SwingWorker;
+import javax.swing.SwingWorker.StateValue;
 
 /**
  *
  * @author sjinadasa
  */
-public class ButtonListener implements ActionListener {
+public class ButtonListener implements ActionListener, PropertyChangeListener {
 
     private JButton button;
     private String btnType;
-    
+
     private JTextField input;
     private JTextArea output;
     private JTextField precision;
-    
-    public ButtonListener(JButton button, String buttonType, JTextField inputField, JTextArea outputField, JTextField precision){
+    private JProgressBar progressBar;
+
+    private findPiToDecimalPlace processPi;
+    private Thread t;
+
+    // For the propertyChange listener
+    public ButtonListener(JProgressBar progressBar, JButton button) {
+        this.progressBar = progressBar;
+    }
+
+    // for Button Listener
+    public ButtonListener(JButton button, String buttonType, JTextField inputField, JTextArea outputField, JTextField precision, JProgressBar progressBar) {
         this.button = button;
         this.btnType = buttonType;
         this.input = inputField;
         this.output = outputField;
         this.precision = precision;
+        this.progressBar = progressBar;
     }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        
-        if(btnType == "process"){
-            try{
-                int decPlace = Integer.parseInt(this.input.getText());
-                
-                int precisionInt = Integer.parseInt(this.precision.getText());
-                if(precisionInt <= 0 || precisionInt > 1000){
-                    throw new IllegalArgumentException();
-                }
-                // Do Pi finding process and print output
-                findPiToDecimalPlace processPi = new findPiToDecimalPlace(decPlace,precisionInt);
-                String piSol = processPi.run();               
-                
-                this.output.setLineWrap(true);
-                this.output.setText(piSol);
-                
-            }catch (NumberFormatException x){
-                this.output.setText("Please enter an Integer");
-            }catch  (IllegalArgumentException I){
-                this.output.setText("Please enter a number between 1 - 1000");
-            }
-            
-        }else if(btnType == "reset"){
+
+        if (btnType == "process") {
+
+            // Runs Pi finder in new thread
+            findPiInNewThread();
+            t.start();
+
+            // What happens if reset is pressed
+        } else if (btnType == "reset") {
             this.input.setText("");
             this.output.setText("");
             this.precision.setText("");
         }
     }
-    
-    
+
+    private void findPiInNewThread() {
+        // Running the pi finding class in a new thread - seperate from the UI
+        t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    int decPlace = Integer.parseInt(input.getText());
+
+                    int precisionInt = Integer.parseInt(precision.getText());
+                    if (precisionInt <= 0 || precisionInt > 1000) {
+                        throw new IllegalArgumentException();
+                    }
+
+                    button.setEnabled(false);
+                    // Do Pi finding process and print output
+                    processPi = new findPiToDecimalPlace(decPlace, precisionInt);
+                    processPi.addPropertyChangeListener(new ButtonListener(progressBar, button));
+                    processPi.execute();
+
+                    String piSol = processPi.get();
+
+                    output.setLineWrap(true);
+                    output.setText(piSol);
+                    button.setEnabled(true);
+
+                } catch (NumberFormatException x) {
+                    output.setText("Please enter an Integer");
+                } catch (IllegalArgumentException I) {
+                    output.setText("Please enter a number between 1 - 1000");
+                } catch (Exception I) {
+                    output.setText("Interrupted - Try Again");
+                }
+            }
+        });
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        if ("progress".equals(evt.getPropertyName())) {
+            int progress = (Integer) evt.getNewValue();
+            this.progressBar.setValue(progress);
+        }
+    }
 }
--- src/find/pi/ui/UserInterface.java
+++ src/find/pi/ui/UserInterface.java
@@ -14,11 +14,13 @@ import static javax.swing.GroupLayout.Alignment.CENTER;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JProgressBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 import javax.swing.border.Border;
+import java.util.*;
 
 /**
  *
@@ -43,31 +45,45 @@ public class UserInterface implements Runnable {
 
     private void createComponents(Container container) {
 
-        GridLayout layoutUI = new GridLayout(4, 2);
+        // Creating the Grid
+        GridLayout layoutUI = new GridLayout(5, 2);
         layoutUI.setHgap(5);
         layoutUI.setVgap(5);
         container.setLayout(layoutUI);
 
+        // The input field
         JTextField inputField = new JTextField();
         JLabel inputLabel = new JLabel("<html> <body style='text-align:center'>Enter the number of "
                 + "decimal places to find Pi to </body> </html>", JLabel.CENTER);
-        
-        JTextField precField = new JTextField();
-        JLabel precLabel = new JLabel("Enter the precision level (1 - 1000 ONLY in this edition)", JLabel.CENTER);
 
+        JTextField precField = new JTextField();
+        JLabel precLabel = new JLabel("<html> <body style='text-align:center'> Enter the precision level (1 - 1000 ONLY in this edition) </body></html>", JLabel.CENTER);
+
+        // Output field is a Text Area inside a ScrollPane cuz of the number of decimals
         JTextArea outputField = new JTextArea();
         JScrollPane outputScroll = new JScrollPane(outputField);
         outputField.setEnabled(false);
         JLabel outputLabel = new JLabel("Pi is ", JLabel.CENTER);
 
+        // Creating a progress Bar
+        JProgressBar progressBar = new JProgressBar(0, 100);
+        progressBar.setValue(0);
+        progressBar.setStringPainted(true);
+
+        // Find Button is created and action listener is added
         JButton findBtn = new JButton("Find Pi");
-        ButtonListener findButtonListener = new ButtonListener(findBtn, "process", inputField,outputField,precField);
+        ButtonListener findButtonListener = new ButtonListener(findBtn, "process", inputField, outputField, precField, progressBar);
         findBtn.addActionListener(findButtonListener);
-        
-        JButton resetBtn = new JButton ("Reset");
-        ButtonListener resetButtonListener = new ButtonListener(resetBtn, "reset", inputField,outputField, precField);
+
+        // Reset button is created and action listener is added
+        JButton resetBtn = new JButton("Reset");
+        ButtonListener resetButtonListener = new ButtonListener(resetBtn, "reset", inputField, outputField, precField, progressBar);
         resetBtn.addActionListener(resetButtonListener);
-        
+
+        // Creating a JLabel to put my name cuz I'm vain
+        JLabel creatorName = new JLabel("<html> <body style='text-align:center'> Created by: Sachintha Jinadasa <br> First compiled on - 02/07/2019 <br> Last change - " + new Date() + "</body></html>", JLabel.CENTER);
+
+        // Everything is added to the container layout
         container.add(inputLabel);
         container.add(inputField);
         container.add(precLabel);
@@ -77,6 +93,8 @@ public class UserInterface implements Runnable {
 
         container.add(findBtn);
         container.add(resetBtn);
+        container.add(progressBar);
+        container.add(creatorName);
     }
 
 }
